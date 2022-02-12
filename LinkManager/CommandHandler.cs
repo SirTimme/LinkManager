275d04a10b25d87b5e9437def8ae3c74b537b197
SirTimme <tim.piecho@gmail.com>
2022-02-12 10:40:56 +0100

    Added Basic Bot Structure

---
+++ LinkManager/CommandHandler.cs
@@ -0,0 +1,137 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace LinkManager
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient client;
+        private readonly InteractionService commands;
+        private readonly IServiceProvider services;
+
+        public CommandHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            this.client = client;
+            this.commands = commands;
+            this.services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+            client.InteractionCreated += HandleInteraction;
+
+            commands.SlashCommandExecuted += SlashCommandExecuted;
+            commands.ContextCommandExecuted += ContextCommandExecuted;
+            commands.ComponentCommandExecuted += ComponentCommandExecuted;
+        }
+
+        private Task ComponentCommandExecuted(ComponentCommandInfo commandInfo, IInteractionContext ctx, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task ContextCommandExecuted(ContextCommandInfo commandInfo, IInteractionContext ctx, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task SlashCommandExecuted(SlashCommandInfo commandInfo, IInteractionContext ctx, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleInteraction(SocketInteraction interaction)
+        {
+            try
+            {
+                var ctx = new SocketInteractionContext(client, interaction);
+                await commands.ExecuteCommandAsync(ctx, services);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+
+                if (interaction.Type == InteractionType.ApplicationCommand)
+                {
+                    await interaction.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+                }
+            }
+        }
+    }
+}
--- LinkManager/Program.cs
+++ LinkManager/Program.cs
@@ -1,1 +1,66 @@
-﻿
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkManager
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            RunAsync(config).GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync(IConfiguration configuration)
+        {
+            using var services = ConfigureServices(configuration);
+
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            var commands = services.GetRequiredService<InteractionService>();
+
+            client.Log += LogAsync;
+            commands.Log += LogAsync;
+
+            client.Ready += async () =>
+            {
+                if (configuration.GetValue<bool>("debug"))
+                {
+                    await commands.RegisterCommandsToGuildAsync(configuration.GetValue<ulong>("devGuild"), deleteMissing: true);
+                }
+                else
+                {
+                    await commands.RegisterCommandsGloballyAsync(true);
+                }
+            };
+
+            await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+            await client.LoginAsync(TokenType.Bot, configuration["token"]);
+            await client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        static Task LogAsync(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+        static ServiceProvider ConfigureServices(IConfiguration config)
+        {
+            return new ServiceCollection()
+                        .AddSingleton(config)
+                        .AddSingleton<DiscordSocketClient>()
+                        .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                        .AddSingleton<CommandHandler>()
+                        .BuildServiceProvider();
+        }
+    }
+}
